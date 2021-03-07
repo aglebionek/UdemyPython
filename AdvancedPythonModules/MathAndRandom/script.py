e96ad8fb0343c56b89dc12a895394577a52ddd99
aglebionek <aglebionek@gmail.com>
2021-03-07 17:47:35 +0100

    just touched the math module

---
+++ AdvancedPythonModules/MathAndRandom/script.py
@@ -0,0 +1,18 @@
+import math
+#help(math)
+
+### rounding
+value = 4.35
+print(math.floor(value))
+print(math.ceil(value))
+print(round(value, 1))
+
+# python will round a number to the closest even number to even the roundings
+v = 4.5
+v2 = 5.5
+print(round(v), round(v2)) # v is rounded to 4, v2 to 6
+###
+
+### stuff
+print(math.log(math.e))
+print(math.degrees(math.pi/2))

e382b7a2c8b17f631ca518dcb5b2cc8add36037a
aglebionek <aglebionek@gmail.com>
2021-03-28 21:14:35 +0200

    web scraping

--- WebScraping/fun.py
+++ WebScraping/fun.py
@@ -1,9 +1,13 @@
 import requests
 import bs4
 
-website = requests.get(url="https://auctions.craftlink.xyz/", allow_redirects=True)
+session = requests.Session()
+website = session.get(url="http://auctions.craftlink.xyz/", allow_redirects=True)
+cookies = dict(website.cookies)
+website = session.get(url="http://auctions.craftlink.xyz/", allow_redirects=True, cookies=cookies, verify=False)
 
-soup = bs4.BeautifulSoup(website.text, "lxml")
-print(soup)
-newest_auctions = soup.find_all('div', attrs={'class': 'bg-gray-900 p-3 rounded-lg shadow-lg'})
-print(newest_auctions[0].gettext())
+#soup = bs4.BeautifulSoup(website.text, "lxml")
+print(website.status_code, website.__dict__.get('location'))
+#print(soup)
+#newest_auctions = soup.find_all('div', attrs={'class': 'bg-gray-900 p-3 rounded-lg shadow-lg'})
+#print(newest_auctions[0].gettext())
--- WebScraping/script.py
+++ WebScraping/script.py
@@ -5,3 +5,8 @@ website = requests.get("http://www.example.com")
 
 soup = bs4.BeautifulSoup(website.text, "lxml")
 print(soup.select('title')[0].getText())
+
+res = requests.get("https://en.wikipedia.org/wiki/Grace_Hopper")
+soup = bs4.BeautifulSoup(res.text,"lxml")
+for item in soup.select('.toctext'): # select elements with class .toctext
+    print(item.text)

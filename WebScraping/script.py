b805e0ced9b2e37147ca1677d46b8a729fa8fca1
aglebionek <aglebionek@gmail.com>
2021-04-01 22:45:26 +0200

    finished book examples

--- WebScraping/script.py
+++ WebScraping/script.py
@@ -23,6 +23,26 @@ f.close()
 
 #GOAL: grab title of every book with 2 star rating
 base_url = "http://books.toscrape.com/catalogue/page-{}.html"
-res = requests.get(base_url.format(1))
-soup = bs4.BeautifulSoup(res.text, "lxml")
-products = soup.select(".product_pod")
+#res = requests.get(base_url.format(1))
+#soup = bs4.BeautifulSoup(res.text, "lxml")
+#products = soup.select(".product_pod")
+#example = products[0]
+#print('star-rating Two' in str(example)) # dirty way
+#print(example.select(".star-rating.Two")) # nicer way, replace spaces with dots
+#example.select('a')[1]['title'] # the 2nd tag contains the title
+
+#carefull, it takes for a while
+two_star_titles = list()
+for n in range(1, 51): # there are 50 pages with books
+    scrape_url = base_url.format(n)
+    res = requests.get(scrape_url)
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    books = soup.select(".product_pod")
+    
+    for book in books: # if the book has a different rating, the book.select list will be empty
+        if book.select(".star-rating.Two"):
+            book_title = book.select('a')[1]['title']
+            two_star_titles.append(book_title)
+
+print(two_star_titles)
+            
